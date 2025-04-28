495b1440260f1a1a31975553dd9af5dfb6ed1107
Zia <ziakayani.official@gmail.com>
2025-04-28 10:14:11 -0700

    OS module ,list, remove, create, rename,direcories

---
+++ python-autom-modules-#5.py
@@ -0,0 +1,76 @@
+#python mdules ------------------
+# sometime you write some code and want to use that in seperate files so we can 
+# use that file as a module
+
+# there are three types of modules
+# 1- user defined
+# 2- built in modules
+# 3- third party modules
+
+#user defined module 
+#myfile.py
+fname="zia"
+
+#otherfile.py
+import myfile
+print(myfile.fname)
+
+#or we can import
+from myfile import *
+print(fname)
+
+#or we can 
+import myfile as zee
+print(zee.fname)
+
+#built in modules
+import os 
+os.system("dir")
+
+os.system("ipconfig")
+dir(os) #for ethernet adapter check
+
+#third party modules 
+#we can install third party modules like pandas , sleenium ,etc etc
+
+#--------------OS module---
+import os 
+print(os.getcwd())
+
+#to  see the current direcorty and change that 
+print(os.getcwd())
+dir_path ="c:\\users" #linux - /home/zia etc
+os.chdir(dir_path)
+print(os.getcwd())
+
+
+#to list the current directories
+print(os.listdir()) #will show the working directories in currect path
+
+# to see the seperator 
+print(f"my windows seperator is {os.sep}")
+
+
+#to make direcory
+os.mkdir("zain")
+print(os.listdir())
+
+#to remove the dir
+os.rmdir("c:\\users\zain")
+print(os.listdidr())
+
+#to rename or remove the file 
+os.rename("zian", "zia")
+print(os.listdir())
+
+#process , group and user ids  in linx
+print("my currnt working dir is", os.getcwd())
+print("my user is id : ", os.getuid())
+print("my group id is ", os.getgid())
+print("my process id is : ", os.getpid())
+
+#Task -- show only .txt file in current work dir in linux /windows
+dir_name =os.getcwd()
+for filename in os.listdir(dir_name):
+    if filename.endswith(".txt"):
+        print(filename)
