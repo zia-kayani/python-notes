61c2a770111ef2822df9a3bcda7b4802b478332a
Zia <ziakayani.official@gmail.com>
2025-05-01 07:23:42 -0700

    convert json to dictionary

--- python-Autom-file-handling1-#15.py
+++ python-Autom-file-handling1-#15.py
@@ -32,7 +32,6 @@ for k,v in my_dict.items():
 ##################
 
 jsonfile =  "E:\Web Development\python learning\os.json"
-
 with open(jsonfile,"r") as jf:
     my_dict= json.load(jf)
    #print(my_dict)
@@ -42,3 +41,98 @@ for i in my_dict:
     for k,v in i.items():
         print(f"My key is {k}")
         print(f"My value is {v}")
+
+#to see specific value based on condition
+with open(jsonfile,"r") as jf:
+    my_dict= json.load(jf)
+    #print(my_dict)
+
+for i in my_dict:
+    #print(i)
+
+    for k,v in i.items():
+        if v == 'Ubuntu':
+            print("My Ubuntu version is ", i["Version"])
+        if v == 'CentOS':
+            print("My CentOS version is ",i['Version'])
+
+
+#How to convert dictionary to JSON data 
+myinfo = {
+	"server1" : { 
+		"IBM": {
+			"datacenter":"Bangalore",
+			"env": {
+				"PR": "192.168.1.1",
+				"DR": "192.168.1.2"
+				}
+			}
+		    }
+	}
+print(myinfo)
+mypath="user_details.json"
+with open(mypath,'w') as df:
+    json.dump(myinfo,df)
+
+
+
+print(type(myinfo))
+
+
+#. ........................ .
+mypath="user_details.json"
+with open(mypath,'w') as df:
+    json.dump(myinfo,df,indent=4) #it gives 4 spaces as an indentation spaces
+#. ..................... .
+
+
+
+#######TASK TIME#######################
+
+mylist= [
+  {
+    "name": "apple",
+    "color": "green",
+    "price": 1.2
+  },
+  {
+    "name": "banana",
+    "color": "yellow",
+    "price": 0.5
+  },
+  {
+    "name": "kiwi",
+    "color": "green",
+    "price": 1.25
+  },
+  { 
+    "name":"grapes",
+    "color": "new",
+    "price": 9.99
+  }
+]
+
+# This is my JSON data so i want to display fruits which is above price 1 
+
+# Output should be like below format:-
+# Total we have 3 fruits with 1 price and fruits name is : Apple Kiwi gra
+
+import json
+mynewpath="fruits.json"
+with open(mynewpath , 'w') as file:
+    json.dump(mylist , file)
+
+newlist =list()
+with open(mynewpath , 'r') as file:
+    data = json.load(file)
+    print(type(data))
+    print(data)
+    
+    for i in data:
+        if i['price'] > 1:
+            newlist.append(i['name'])
+print(f"Total we have 3 fruits with 1 price and fruits name is  {newlist[0]} {newlist[1]} {newlist[2]}")    
+    
+
+
+
