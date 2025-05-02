a898785a4104584910326c170044223d5152eed1
Zia <ziakayani.official@gmail.com>
2025-05-02 10:41:00 -0700

    send attachment, logo, file in the mail

--- python-Autom-emailmodule-#16.py
+++ python-Autom-emailmodule-#16.py
@@ -93,3 +93,161 @@ msg.add_attachment(attach,maintype='application',subtype='octet-stream',filename
 connection.send_message(msg)
 connection.close()
 print("Mail has been sent")
+
+
+
+
+
+
+
+#We will use html format in body 
+import smtplib
+from email.message import EmailMessage
+
+mymail = "ziakayani.official@gmail.com"
+password = "kjac ygho sgsr fdrr"
+connection =smtplib.SMTP('smtp.gmail.com')
+connection.starttls()
+connection.login(user=mymail , password=password)
+
+msg = EmailMessage()
+msg['Subject']= "Hello team "
+msg['From']=mymail
+msg['To']='ziakayani888@gmail.com'
+msg['Cc']=['ziakayani888@gmail.com', 'ziakayani.official@gmail.com']
+
+msg.add_alternative("""
+                    <html> 
+                        <b>hello team </b>
+                        <p> messg from zia,</p>
+                    </html>
+                    """,subtype='html')
+connection.send_message(msg)
+connection.close()
+print('mail sent succes')
+
+
+
+
+# NEW MIME Method to send email
+
+# MIME --> Multipurpose Internet Mail Extensions is internet standard thant extedns the format of email messages to supports text in characters set of other the ascii 
+# suchh au audio , video , images and application prgram
+
+
+# MIME (Multipurpose Internet Mail Extensions) is an extension of the original Simple Mail Transport Protocol (SMTP) email protocol. It lets users exchange different kinds of data files, including audio, video, images and application programs, over email.
+
+# MIMEMultipart --> its saying i have more than one part ans listing the parts , you can implemtn text and html version
+
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import smtplib
+
+mymail = "ziakayani.official@gmail.com"
+password = "kjac ygho sgsr fdrr"
+connection =smtplib.SMTP('smtp.gmail.com')
+connection.starttls()
+connection.login(user=mymail , password=password)
+
+msg = MIMEMultipart()
+msg['Subject']= "Hello team "
+msg['From']=mymail
+msg['To']='ziakayani888@gmail.com'
+msg['Cc']=''.join(['ziakayani888@gmail.com', 'ziakayani.official@gmail.com'])
+
+body=""" 
+<html><p> <b> <i>Hi Team,<br> This is testing email server via python script. <b> <i></p> </html> 
+"""
+msg.attach(MIMEText(body , 'html'))
+
+connection.send_message(msg)
+connection.close()
+print('mail sent succes')
+
+
+
+#sending attachment file 
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
+
+mymail = "ziakayani.official@gmail.com"
+password = "kjac ygho sgsr fdrr"
+connection =smtplib.SMTP('smtp.gmail.com')
+connection.starttls()
+connection.login(user=mymail , password=password)
+
+msg = MIMEMultipart()
+msg['Subject']= "Hello team "
+msg['From']=mymail
+msg['To']='ziakayani888@gmail.com'
+msg['Cc'] = ', '.join(['ziakayani.official@gmail.com', 'ziakayani888@gmail.com'])
+
+body=""" 
+<html><p> <b> <i>Hi Team,<br> This is testing email server via python script. <b> <i></p> </html> 
+"""
+msg.attach(MIMEText(body,'html'))
+
+#attachment section
+filename= r'E:\Web Development\python learning\zia.txt'
+attachment = open(filename , 'rb')
+part = MIMEBase('application' , 'octet-stream')
+part.set_payload((attachment).read())
+encoders.encode_base64(part)
+part.add_header('Content-Disposition', "attachment; filename=%s" %filename)
+msg.attach(part)
+
+connection.send_message(msg)
+connection.close()
+print("Mail has been sent")
+
+
+
+#sending logo as well l 
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
+from email.mime.image import MIMEImage
+
+mymail = "ziakayani.official@gmail.com"
+password = "kjac ygho sgsr fdrr"
+connection =smtplib.SMTP('smtp.gmail.com')
+connection.starttls()
+connection.login(user=mymail , password=password)
+
+msg = MIMEMultipart()
+msg['Subject']= "Hello team "
+msg['From']=mymail
+msg['To']='ziakayani888@gmail.com'
+msg['Cc'] = ', '.join(['ziakayani.official@gmail.com', 'ziakayani888@gmail.com'])
+
+body=""" 
+<html><p> <b> <i>Hi Team,<br> This is testing email server via python script.  <b> <i> <br> <img src="cid:alogo" width="100" height="50"></p> </html> 
+"""
+msg.attach(MIMEText(body,'html'))
+
+#logo section
+file = 'E:\Web Development\python learning\logo.jpg'
+mylogo= open(file , 'rb')
+msgIMAGE= MIMEImage(mylogo.read())
+mylogo.close()
+msgIMAGE.add_header('Content-ID', 'alogo')
+msg.attach(msgIMAGE)
+
+
+#attachment section
+filename= r'E:\Web Development\python learning\zia.txt'
+attachment = open(filename , 'rb')
+part = MIMEBase('application' , 'octet-stream')
+part.set_payload((attachment).read())
+encoders.encode_base64(part)
+part.add_header('Content-Disposition', "attachment; filename=%s" %filename)
+msg.attach(part)
+
+connection.send_message(msg)
+connection.close()
+print("Mail has been sent")
