feb76b75b2d439d7b96b57b8215d35b4fa621c4b
Zia <ziakayani.official@gmail.com>
2025-05-13 11:24:55 -0700

    databse module

---
+++ python-Autom-database-module-#20.py
@@ -0,0 +1,16 @@
+#------ Database Module -----------
+
+# -- install sql-server -----
+#step1:- #apt-get update
+#step2:- apt-get install mysql-server
+#step3:- 
+# systemctl status mysql.service
+# systemctl enable mysql.service
+# systemctl start mysql.service
+#step4:- #Now you need to confiugre your mysql server
+# mysql_secure_installation
+#step5-: login
+# mysql -u root -p
+
+#to check database   show databases;
+#to create database   create database <db_name>;
