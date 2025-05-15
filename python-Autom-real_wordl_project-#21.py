9394e1b205467e94a0d59c2ed04d13a91a5d891f
Zia <ziakayani.official@gmail.com>
2025-05-15 12:34:25 -0700

    password encryption and database table creation

--- python-Autom-real_wordl_project-#21.py
+++ python-Autom-real_wordl_project-#21.py
@@ -124,3 +124,105 @@ udev,953M,0,953M,0,/dev,2022-10-02 11:30:45,192.168.1.8,abdeali.local
 /dev/sda2,875M,149M,665M,19,/boot,2022-10-02 11:30:45,192.168.1.8,abdeali.local
 
 
+###################################################################################3
+#Now ltes intsert the data in database  and encripyt out password
+
+#create a seperate file of commands like - mylinux.json
+root@zeekayani:/home/Documents# cat mylinux.json 
+{
+        "username": "mysql_user",
+        "password": "test123",
+        "os_flavour": "cat /etc/os-release | grep -iw \"NAME\" | awk -F = '{print $2}' | tr -d '\"'",
+        "df_cmd" : "df -h  | grep -v 'tmpfs' | awk 'NR!=1' | awk '{print $1,$2,$3,$4,$5,$6}' | sed -e 's/%//g' | sed -E \"s/ +/,/g\" | sed \"s/$/,$(date '+%F %T')/g\" | sed \"s/$/,$(hostname -I | awk '{print $1}')/g\" |  sed \"s/$/,$(hostname)/g\" > myfilecsv.csv"
+}
+
+#now lets use that file instead of commands for better code visibility 
+import os
+import json
+
+try:
+    myjsonfile = "mylinux.json"
+    with open(myjsonfile, "r") as jf:
+        my_dict = json.load(jf)
+        os_name = os.popen(my_dict['os_flavour']).read().strip('\n')
+        if os_name == 'Ubuntu' or os_name == 'redhat':
+            df_cmd = os.popen(my_dict['df_cmd']).read()
+            print(df_cmd)
+        else:
+            print("Other os found", os_name)
+except Exception as e:
+    print("Something having issue", e)
+
+
+###now we will create table in database
+CREATE TABLE my_df_data (
+    file_id INT(11) NOT NULL AUTO_INCREMENT,
+    filesystem VARCHAR(255),
+    size VARCHAR(255),
+    used VARCHAR(255),
+    avail VARCHAR(255),
+    usage_with_per INT,
+    mounted_on VARCHAR(255),
+    datetime DATETIME,
+    ip_address VARCHAR(255),
+    hostname VARCHAR(255),
+    PRIMARY KEY (file_id)
+);
+
+#now login with mysql user
+mysql -u mysql_user -h 192.168.75.62 -pass
+
+#use alnafi databse
+
+#in this create the table with my_df_data name
+
+#now insert manually values in this
+INSERT INTO my_df_data 
+(filesystem, size, used, avail, usage_with_per, mounted_on, datetime, ip_address, hostname) 
+VALUES 
+('/dev/sda2', '875M', '1:49M', '565M', 13, '/boot', '2022-10-02 13:14:05', '192.168.1.6', 'abdeal');
+
+
+
+### now write a code for your password encryption and decryption
+import json
+from cryptography.fernet import Fernet
+
+jsonfile = 'mypass.json'
+
+# Load JSON data
+with open(jsonfile) as jf:
+    my_dict = json.load(jf)
+
+# Display username and password
+print("This is my username: " + my_dict['username'])
+print("This is my Password: " + my_dict['password'])
+
+mypass = my_dict['password']
+print(mypass)
+
+# Encrypt the password
+message = mypass.encode("utf-8")
+print("Encoded message:", message)
+
+key = Fernet.generate_key()
+print("Generated key:", key)
+
+f = Fernet(key)
+print("Fernet object:", f)
+
+encrypted = f.encrypt(message)
+print("Encrypted password:", encrypted)
+
+# Decrypt the password
+decrypted = f.decrypt(encrypted)
+print("Decrypted bytes:", decrypted)
+
+my_new_pass = decrypted.decode("utf-8")
+print("Decrypted password:", my_new_pass)
+
+
+
+
+
+
